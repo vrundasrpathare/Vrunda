7d740b21fd8769a83e13b421c6065333513c3ca0
vrundasrpathare <113424604+vrundasrpathare@users.noreply.github.com>
2022-09-13 18:43:24 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 	
 	@GetMapping
 	public String sayHello() {
-		return "The website is up and running first commit 888 ***!!!";
+		return "The website is up and running first commit 999 ***!!!";
 	}
 
 }
